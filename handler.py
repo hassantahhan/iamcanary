b22566d2839d1f837ea622735cb77ea60efb394f
Hassan Tahhan <45363469+hassantahhan@users.noreply.github.com>
2021-05-15 20:59:43 +1000

    Add files via upload

--- handler.py
+++ handler.py
@@ -1,25 +1,54 @@
 import os
 import boto3
 
-def iam_canary_check():
+# check if principal is allowed on all actions
+def check_all_allowed():
 	source_arn = os.environ.get('source_arn')
 	action_names = os.environ.get('action_names')
 
 	client = boto3.client('iam')
 	result = client.simulate_principal_policy(PolicySourceArn=source_arn, ActionNames=tuple(action_names.split(',')))
 
-	allowed = None
+	all_allowed = None
+
 	for decision in (result['EvaluationResults']):
-		if decision['EvalDecision'] == 'allowed' and allowed == None:
-			allowed = True
+		if all_allowed == None:
+			all_allowed = decision['EvalDecision'] == 'allowed'
 		elif 'Deny' in decision['EvalDecision']:
-			allowed = False
+			all_allowed = False
 
-	value_string = '[allowed=' + str(allowed) + ', source_arn=' + source_arn + ', action_names=' + action_names +']'	
+	value_string = 'all_allowed=' + str(all_allowed) + ', source_arn=' + source_arn + ', action_names=' + action_names
 
-	if not allowed:
-		raise Exception('Failed iam_canary_check ' + value_string)
-	return 'Successfully completed iam_canary_check ' + value_string
+	if not all_allowed:
+		raise Exception('Failed IAM Canary Test: ' + value_string)
+	return 'Completed IAM Canary Test: ' + value_string
 
-def handler(event, context): 
-    return iam_canary_check()
+# check if principal is denied on all actions
+def check_all_denied():
+	source_arn = os.environ.get('source_arn')
+	action_names = os.environ.get('action_names')
+
+	client = boto3.client('iam')
+	result = client.simulate_principal_policy(PolicySourceArn=source_arn, ActionNames=tuple(action_names.split(',')))
+
+	all_denied = None
+	
+	for decision in (result['EvaluationResults']):
+		if all_denied == None:
+			all_denied = 'Deny' in decision['EvalDecision']
+		elif decision['EvalDecision'] == 'allowed':
+			all_denied = False
+
+	value_string = 'all_denied=' + str(all_denied) + ', source_arn=' + source_arn + ', action_names=' + action_names
+
+	if not all_denied:
+		raise Exception('Failed IAM Canary Test: ' + value_string)
+	return 'Completed IAM Canary Test: ' + value_string
+
+# lambda handler for action allowed canary test 
+def canary_test_allowed(event, context): 
+    return check_all_allowed()
+
+# lambda handler for action denied canary test 
+def canary_test_denied(event, context): 
+    return check_all_denied()
--- test.py
+++ test.py
@@ -2,13 +2,39 @@ import os
 import handler
 
 def test_iam_canary_check():
-	print('### Test 1: handler.iam_canary_check...')
-	os.environ['source_arn'] = 'arn:aws:iam::111111111111:role/Admin' # Change this to your principal arn
-	os.environ['action_names'] = 'ec2:RunInstances'
-	print(handler.iam_canary_check())
-	os.environ['action_names'] = 'ec2:RunInstances,imagebuilder:CreateImagePipeline'
-	print('### Test 2: iam_canary_check...')
-	print(handler.iam_canary_check())
+	print('### Test Start!')
+
+	# Change source_arn to your principal arn
+	os.environ['source_arn'] = 'arn:aws:iam::111111111111:role/Admin' 
+		
+	print('### Test 1: check_all_allowed...')
+	try:
+		os.environ['action_names'] = 'ec2:RunInstances'
+		print(handler.check_all_allowed())
+	except Exception as e:
+		print(e)
+
+	print('### Test 2: check_all_allowed...')
+	try:
+		os.environ['action_names'] = 'ec2:RunInstances,imagebuilder:CreateImagePipeline'
+		print(handler.check_all_allowed())
+	except Exception as e:
+		print(e)
+	
+	print('### Test 3: check_all_denied...')
+	try:
+		os.environ['action_names'] = 'imagebuilder:CreateImagePipeline'
+		print(handler.check_all_denied())
+	except Exception as e:
+		print(e)
+		
+	print('### Test 4: check_all_denied...')
+	try:
+		os.environ['action_names'] = 'ec2:RunInstances,imagebuilder:CreateImagePipeline'
+		print(handler.check_all_denied())
+	except Exception as e:
+		print(e)
+
 	print('### Test End!')
 
 if __name__ == '__main__':
