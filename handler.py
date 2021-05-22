579d413b8c2bd83442deb2042467f1e2e7215f3a
Hassan Tahhan <45363469+hassantahhan@users.noreply.github.com>
2021-05-22 23:44:24 +1000

    Add files via upload

--- handler.py
+++ handler.py
@@ -1,54 +1,42 @@
 import os
+import json
 import boto3
 
-# check if principal is allowed on all actions
-def check_all_allowed():
-	source_arn = os.environ.get('source_arn')
-	action_names = os.environ.get('action_names')
-
-	client = boto3.client('iam')
-	result = client.simulate_principal_policy(PolicySourceArn=source_arn, ActionNames=tuple(action_names.split(',')))
-
-	all_allowed = None
-
-	for decision in (result['EvaluationResults']):
-		if all_allowed == None:
-			all_allowed = decision['EvalDecision'] == 'allowed'
-		elif 'Deny' in decision['EvalDecision']:
-			all_allowed = False
-
-	value_string = 'all_allowed=' + str(all_allowed) + ', source_arn=' + source_arn + ', action_names=' + action_names
-
-	if not all_allowed:
-		raise Exception('Failed IAM Canary Test: ' + value_string)
-	return 'Completed IAM Canary Test: ' + value_string
-
-# check if principal is denied on all actions
-def check_all_denied():
-	source_arn = os.environ.get('source_arn')
-	action_names = os.environ.get('action_names')
-
-	client = boto3.client('iam')
-	result = client.simulate_principal_policy(PolicySourceArn=source_arn, ActionNames=tuple(action_names.split(',')))
-
-	all_denied = None
-	
-	for decision in (result['EvaluationResults']):
-		if all_denied == None:
-			all_denied = 'Deny' in decision['EvalDecision']
-		elif decision['EvalDecision'] == 'allowed':
-			all_denied = False
-
-	value_string = 'all_denied=' + str(all_denied) + ', source_arn=' + source_arn + ', action_names=' + action_names
-
-	if not all_denied:
-		raise Exception('Failed IAM Canary Test: ' + value_string)
-	return 'Completed IAM Canary Test: ' + value_string
-
-# lambda handler for action allowed canary test 
+# lambda handler for canary test of allowed actions 
 def canary_test_allowed(event, context): 
-    return check_all_allowed()
+    return check_all_principals_actions(True)
 
-# lambda handler for action denied canary test 
+# lambda handler for canary test of denied actions
 def canary_test_denied(event, context): 
-    return check_all_denied()
+    return check_all_principals_actions(False)
+
+# check by order if all principals have the allowed or denied effect on all actions
+def check_all_principals_actions(effect_allow):
+    input_json = os.environ.get("principals_actions_json")
+    dictionary = json.loads(input_json)
+
+    for key in dictionary:
+        check_one_principal_actions(key, dictionary[key], effect_allow)
+ 
+    if effect_allow:
+        return "Completed IAM Canary Test of Allowed Actions: " + input_json   
+    else:
+        return "Completed IAM Canary Test of Denied Actions: " + input_json
+
+# check if one principal is allowed (effect_allow = true) or denied (effect_allow = false) on all actions
+def check_one_principal_actions(principal_arn, action_names, effect_allow):
+    client = boto3.client("iam")
+    result = client.simulate_principal_policy(PolicySourceArn=principal_arn, ActionNames=tuple(action_names.split(",")))
+
+    input_string = "principal_arn=" + principal_arn + ", action_names=" + action_names
+
+    for decision in (result["EvaluationResults"]):
+        if effect_allow and "Deny" in decision["EvalDecision"]:
+            raise Exception("Failed IAM Canary Test of Allowed Actions: " + input_string)
+        elif not effect_allow and decision["EvalDecision"] == "allowed":
+            raise Exception("Failed IAM Canary Test of Denied Actions: " + input_string)
+        
+    if effect_allow:
+        return "Completed IAM Canary Test of Allowed Actions: " + input_string   
+    else:
+        return "Completed IAM Canary Test of Denied Actions: " + input_string
--- test.py
+++ test.py
@@ -1,41 +1,62 @@
 import os
 import handler
 
-def test_iam_canary_check():
-	print('### Test Start!')
+# Change test_principal, test_actions, and test_full_json to match your environment
+test_principal = "arn:aws:iam::111111111111:role/Admin"
+test_1_actions = "ec2:RunInstances"
+test_2_actions = "ec2:RunInstances,imagebuilder:CreateImagePipeline"
+test_3_actions = "imagebuilder:CreateImagePipeline"
+test_4_actions = "imagebuilder:CreateImagePipeline,ec2:RunInstances"
+test_full_json = '{"arn:aws:iam::111111111111:role/Admin":"imagebuilder:CreateImagePipeline",' \
+                 '"arn:aws:iam::111111111111:role/Read":"imagebuilder:CreateImagePipeline"}'
 
-	# Change source_arn to your principal arn
-	os.environ['source_arn'] = 'arn:aws:iam::111111111111:role/Admin' 
-		
-	print('### Test 1: check_all_allowed...')
-	try:
-		os.environ['action_names'] = 'ec2:RunInstances'
-		print(handler.check_all_allowed())
-	except Exception as e:
-		print(e)
+def test_check_one_principal_actions():
+    print("### test_check_one_principal_actions() started")
+        
+    print("? Test 1: check_one_principal_actions(allowed)...")
+    try:
+        print(handler.check_one_principal_actions(test_principal, test_1_actions, True))
+    except Exception as e:
+        print(e)
 
-	print('### Test 2: check_all_allowed...')
-	try:
-		os.environ['action_names'] = 'ec2:RunInstances,imagebuilder:CreateImagePipeline'
-		print(handler.check_all_allowed())
-	except Exception as e:
-		print(e)
-	
-	print('### Test 3: check_all_denied...')
-	try:
-		os.environ['action_names'] = 'imagebuilder:CreateImagePipeline'
-		print(handler.check_all_denied())
-	except Exception as e:
-		print(e)
-		
-	print('### Test 4: check_all_denied...')
-	try:
-		os.environ['action_names'] = 'ec2:RunInstances,imagebuilder:CreateImagePipeline'
-		print(handler.check_all_denied())
-	except Exception as e:
-		print(e)
+    print("? Test 2: check_one_principal_actions(allowed)...")
+    try:
+        print(handler.check_one_principal_actions(test_principal, test_2_actions, True))
+    except Exception as e:
+        print(e)
+    
+    print("? Test 3: check_one_principal_actions(denied)...")
+    try:
+        print(handler.check_one_principal_actions(test_principal, test_3_actions, False))
+    except Exception as e:
+        print(e)
+        
+    print("? Test 4: check_one_principal_actions(denied)...")
+    try:
+        print(handler.check_one_principal_actions(test_principal, test_4_actions, False))
+    except Exception as e:
+        print(e)
 
-	print('### Test End!')
+    print("### test_check_one_principal_actions() ended!")
 
-if __name__ == '__main__':
-    test_iam_canary_check()
+def test_check_all_principals_actions():
+    print("### test_check_all_principals_actions() started")
+    os.environ['principals_actions_json'] = test_full_json
+        
+    print("? Test 1: check_all_principals_actions(allowed)...")
+    try:
+        print(handler.check_all_principals_actions(True))
+    except Exception as e:
+        print(e)
+
+    print("? Test 2: check_all_principals_actions(denied)...")
+    try:
+        print(handler.check_all_principals_actions(False))
+    except Exception as e:
+        print(e)
+        
+    print("### test_check_all_principals_actions() ended!")
+
+if __name__ == "__main__":
+    test_check_one_principal_actions()
+    test_check_all_principals_actions()
