844a74f866f0602e1832d744efb18eafb562204e
Hassan Tahhan <45363469+hassantahhan@users.noreply.github.com>
2021-06-02 21:14:46 +1000

    Add files via upload

--- handler.py
+++ handler.py
@@ -2,41 +2,43 @@ import os
 import json
 import boto3
 
-# lambda handler for canary test of allowed actions 
-def canary_test_allowed(event, context): 
-    return check_all_principals_actions(True)
-
-# lambda handler for canary test of denied actions
-def canary_test_denied(event, context): 
-    return check_all_principals_actions(False)
+# lambda handler for canary test of allowed and denied actions 
+def check_principals_actions(event, context): 
+    return read_and_simulate_principals_actions()
 
 # check by order if all principals have the allowed or denied effect on all actions
-def check_all_principals_actions(effect_allow):
+def read_and_simulate_principals_actions():
     input_json = os.environ.get("principals_actions_json")
     dictionary = json.loads(input_json)
+    
+    allowed_pairs = dict()
+    denied_pairs = dict()
 
-    for key in dictionary:
-        check_one_principal_actions(key, dictionary[key], effect_allow)
- 
-    if effect_allow:
-        return "Completed IAM Canary Test of Allowed Actions: " + input_json   
-    else:
-        return "Completed IAM Canary Test of Denied Actions: " + input_json
+    if "allowed_pairs" in dictionary:
+        allowed_pairs = dictionary["allowed_pairs"]
 
+    if "denied_pairs" in dictionary:
+        denied_pairs = dictionary["denied_pairs"]    
+
+    for key in allowed_pairs:
+        simulate_principal_actions(key, allowed_pairs[key], True)
+
+    for key in denied_pairs:
+        simulate_principal_actions(key, denied_pairs[key], False)
+
+    return "Completed test of " + str(len(allowed_pairs)) + " allowed principal-actions pairs and " + str(len(denied_pairs)) + " denied principal-actions pairs"     
+    
 # check if one principal is allowed (effect_allow = true) or denied (effect_allow = false) on all actions
-def check_one_principal_actions(principal_arn, action_names, effect_allow):
+def simulate_principal_actions(principal_arn, action_names, effect_allow):
     client = boto3.client("iam")
     result = client.simulate_principal_policy(PolicySourceArn=principal_arn, ActionNames=tuple(action_names.split(",")))
 
-    input_string = "principal_arn=" + principal_arn + ", action_names=" + action_names
+    input_string = "effect_allow=" + str(effect_allow) + ", principal_arn=" + principal_arn + ", action_names=" + action_names
 
     for decision in (result["EvaluationResults"]):
         if effect_allow and "Deny" in decision["EvalDecision"]:
-            raise Exception("Failed IAM Canary Test of Allowed Actions: " + input_string)
+            raise Exception("Failed allow test: " + input_string)
         elif not effect_allow and decision["EvalDecision"] == "allowed":
-            raise Exception("Failed IAM Canary Test of Denied Actions: " + input_string)
+            raise Exception("Failed deny test: " + input_string)
         
-    if effect_allow:
-        return "Completed IAM Canary Test of Allowed Actions: " + input_string   
-    else:
-        return "Completed IAM Canary Test of Denied Actions: " + input_string
+    return "Completed test: " + input_string
--- test.py
+++ test.py
@@ -1,62 +1,81 @@
 import os
+import json
 import handler
 
 # Change test_principal, test_actions, and test_full_json to match your environment
-test_principal = "arn:aws:iam::111111111111:role/Admin"
-test_1_actions = "ec2:RunInstances"
-test_2_actions = "ec2:RunInstances,imagebuilder:CreateImagePipeline"
-test_3_actions = "imagebuilder:CreateImagePipeline"
-test_4_actions = "imagebuilder:CreateImagePipeline,ec2:RunInstances"
-test_full_json = '{"arn:aws:iam::111111111111:role/Admin":"imagebuilder:CreateImagePipeline",' \
-                 '"arn:aws:iam::111111111111:role/Read":"imagebuilder:CreateImagePipeline"}'
-
-def test_check_one_principal_actions():
-    print("### test_check_one_principal_actions() started")
+def test_simulate_principal_actions():
+    print("\n### test_simulate_principal_actions() started")
         
-    print("? Test 1: check_one_principal_actions(allowed)...")
+    print("\nTest 1: simulate_principal_actions(allowed)...")
     try:
-        print(handler.check_one_principal_actions(test_principal, test_1_actions, True))
+        test_principal = "arn:aws:iam::111111111111:role/EC2Admin"
+        test_actions = "ec2:RunInstances"
+        print(handler.simulate_principal_actions(test_principal, test_actions, True))
     except Exception as e:
         print(e)
 
-    print("? Test 2: check_one_principal_actions(allowed)...")
+    print("\nTest 2: simulate_principal_actions(allowed)...")
     try:
-        print(handler.check_one_principal_actions(test_principal, test_2_actions, True))
+        test_principal = "arn:aws:iam::111111111111:role/EC2Admin"
+        test_actions = "imagebuilder:CreateImagePipeline"
+        print(handler.simulate_principal_actions(test_principal, test_actions, True))
     except Exception as e:
         print(e)
     
-    print("? Test 3: check_one_principal_actions(denied)...")
+    print("\nTest 3: simulate_principal_actions(allowed)...")
     try:
-        print(handler.check_one_principal_actions(test_principal, test_3_actions, False))
+        test_principal = "arn:aws:iam::111111111111:role/EC2Admin"
+        test_actions = "ec2:RunInstances,imagebuilder:CreateImagePipeline"
+        print(handler.simulate_principal_actions(test_principal, test_actions, True))
     except Exception as e:
         print(e)
         
-    print("? Test 4: check_one_principal_actions(denied)...")
+    print("\nTest 4: simulate_principal_actions(denied)...")
     try:
-        print(handler.check_one_principal_actions(test_principal, test_4_actions, False))
+        test_principal = "arn:aws:iam::111111111111:role/EC2Admin"
+        test_actions = "imagebuilder:CreateImagePipeline,ec2:RunInstances"
+        print(handler.simulate_principal_actions(test_principal, test_actions, False))
     except Exception as e:
         print(e)
 
-    print("### test_check_one_principal_actions() ended!")
+    print("\nTest 5: simulate_principal_actions(denied)...")
+    try:
+        test_principal = "arn:aws:iam::111111111111:role/EC2Admin"
+        test_actions = "ec2:RunInstances,imagebuilder:CreateImagePipeline"
+        print(handler.simulate_principal_actions(test_principal, test_actions, False))
+    except Exception as e:
+        print(e)
 
-def test_check_all_principals_actions():
-    print("### test_check_all_principals_actions() started")
-    os.environ['principals_actions_json'] = test_full_json
+    print("\nTest 6: simulate_principal_actions(denied)...")
+    try:
+        test_principal = "arn:aws:iam::111111111111:role/EC2Run"
+        test_actions = "imagebuilder:CreateImagePipeline"
+        print(handler.simulate_principal_actions(test_principal, test_actions, False))
+    except Exception as e:
+        print(e)
+    print("\n### test_simulate_principal_actions() ended!")
+
+def test_read_and_simulate_principals_actions():
+    print("\n### test_read_and_simulate_principals_actions() started")
+    test_json = {
+                    "allowed_pairs": {
+                        "arn:aws:iam::111111111111:role/EC2Admin": "ec2:RunInstances,imagebuilder:CreateImagePipeline",
+                        "arn:aws:iam::111111111111:role/EC2Run": "ec2:RunInstances"
+                    },
+                    "denied_pairs": {
+                        "arn:aws:iam::111111111111:role/EC2Run": "imagebuilder:CreateImagePipeline"
+                    }
+                }
+    os.environ['principals_actions_json'] = json.dumps(test_json)
+    
+    print("\nTest 1: read_and_simulate_principals_actions()...")
+    try:
+        print(handler.read_and_simulate_principals_actions())
+    except Exception as e:
+        print(e)
         
-    print("? Test 1: check_all_principals_actions(allowed)...")
-    try:
-        print(handler.check_all_principals_actions(True))
-    except Exception as e:
-        print(e)
-
-    print("? Test 2: check_all_principals_actions(denied)...")
-    try:
-        print(handler.check_all_principals_actions(False))
-    except Exception as e:
-        print(e)
-        
-    print("### test_check_all_principals_actions() ended!")
+    print("\n### test_read_and_simulate_principals_actions() ended!")
 
 if __name__ == "__main__":
-    test_check_one_principal_actions()
-    test_check_all_principals_actions()
+    test_simulate_principal_actions()
+    test_read_and_simulate_principals_actions()
